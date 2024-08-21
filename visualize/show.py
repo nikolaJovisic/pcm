63abe34a64ab3bc0bcec23312ab0279c1b12dc0b
Nikola Jovišić <nikolajovisic4@gmail.com>
2024-08-21 16:22:41 +0200

    fix one image show

--- visualize/show.py
+++ visualize/show.py
@@ -1,10 +1,17 @@
+import matplotlib
+# matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 
-
-def show(*images):
+def show(*images, filename='tmp'):
     num_images = len(images)
-    _, axs = plt.subplots(1, num_images, figsize=(5 * num_images, 5))
-    for i, image in enumerate(images):
-        axs[i].imshow(image, cmap="gray")
-        axs[i].axis("off")
+    if num_images == 1:
+        plt.figure(figsize=(5, 5))
+        plt.imshow(images[0], cmap="gray")
+        plt.axis("off")
+    else:
+        _, axs = plt.subplots(1, num_images, figsize=(5 * num_images, 5))
+        for i, image in enumerate(images):
+            axs[i].imshow(image, cmap="gray")
+            axs[i].axis("off")
+    plt.savefig(filename + '.png')
     plt.show()
