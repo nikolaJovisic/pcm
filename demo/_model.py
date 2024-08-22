b576dcbbda8f45bfef38297b0f4ac6a2400852af
Nikola Jovišić <nikolajovisic4@gmail.com>
2024-08-22 14:32:49 +0200

    refactor weights loading

--- demo/_model.py
+++ demo/_model.py
@@ -9,7 +9,7 @@ def _get_weights_path():
     return "model/model_131999.pt"
 
 
-def initialized_inference():
+def uninitialized_inference():
     model = Unet(dim=128, dim_mults=(1, 2, 2, 4, 4), channels=3)
     model.eval()
     x = torch.rand(1, 3, 256, 256)
@@ -17,13 +17,14 @@ def initialized_inference():
         ic(model(x))
 
 
-def weights_loading():
+def weights_loaded_inference():
     model = get_mammo_unet(_get_weights_path())
+    model.eval()
     x = torch.rand(1, 3, 256, 256)
     with torch.no_grad():
         ic(model(x))
 
 
 if __name__ == "__main__":
-    # initialized_inference()
-    weights_loading()
+    # uninitialized_inference()
+    weights_loaded_inference()
